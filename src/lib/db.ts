0d75cb38933088359e983aabcb2c44da2d45656f
robertocommit <r@milhos.tech>
2024-10-07 22:28:53 +0200

    fix db filesystem

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,12 +1,19 @@
 import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 let db: any;
 
 export async function getDb() {
   if (!db) {
+    const dbPath = path.join(__dirname, '..', '..', 'database.sqlite');
+    
     db = await open({
-      filename: 'database.sqlite',
+      filename: dbPath,
       driver: sqlite3.Database
     });
 
